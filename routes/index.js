f1ffad78684fbb9be51fb2f4f98fde1d79509dd0
iriquintero <iriangel.quintero@gmail.com>
2020-12-17 12:03:51 -0300

    proceso de contacto-nodemail

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
+router.post('/', async (req, res, next)=>{
+  var nombre = req.body.nombre;
+  var email = req.body.email;
+  var mensaje = req.body.mensaje;
+  var obj={
+    to:'iriangel.quintero@gmail.com',
+    subject:'contacto desde la web',
+    html: nombre +"se contacto a través de la página web y dejo el siguiente mensaje"+mensaje+" y su email en el caso que necesite escribirle"+email
+  }
+  var transport = nodemailer.createTransport({
+    host:process.env.SMTP_HOST,
+    port:process.env.SMTP_PORT,
+    auth:{
+      user:process.env.SMTP_USER,
+      pass:process.env.SMTP_PASS
+    }
+  })
+  var info = await transport.sendMail(obj);
+    res.render('index',{
+      mensaje:'mensaje enviado'
+    });
+});
 module.exports = router;
